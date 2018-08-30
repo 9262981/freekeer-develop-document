25e0ceb6e30297beb1c9110556ad68b28a05d258
simonsun <9262981@qq.com>
2018-08-30 17:19:33 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app" >
-    <div v-bind:style="styleObject">
+    <el-container>
+      <el-header style="color:#303133;font-size:21px;font-weight:900;"><img alt="FreekeerDevelopDocument" src="./assets/logofreekeer.png" style="width:180px;height:39px;margin-top:10px">-Develop-Document</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>Main</el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+    <!-- <div v-bind:style="styleObject">
       <img alt="Vue logo" style="" src="./assets/logo.png">
       <img alt="Vue logo" style="" src="./assets/logo.png">
       <img alt="Vue logo" style="" src="./assets/logo.png">
@@ -11,7 +21,7 @@
       <img alt="Vue logo" style="" src="./assets/logo.png">
     </div>
 
-    <HelloWorld style="width:80%;float:right" msg="Welcome to Your Vue.js App1"/>
+    <HelloWorld style="width:80%;float:right" msg="Welcome to Your Vue.js App1"/> -->
   </div>
 </template>
 
@@ -54,4 +64,41 @@ body{
   margin:0;
   padding:0;
 }
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: left;
+    line-height: 60px;
+  }
+.el-header{
+    background-color: #fff;
+    box-shadow: 0 10px 60px 0 rgba(29,29,31,0.07);
+    opacity: 0.98;
+}
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
