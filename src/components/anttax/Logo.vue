da54572d43cf708a8dc1b8777f9d0e678cbd21e5
simonsun <9262981@qq.com>
2018-09-21 18:04:06 +0800

    LOGO

--- src/components/anttax/Logo.vue
+++ src/components/anttax/Logo.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="hello">
-  Logo
-    <img :src="img">
-    <img src='../../assets/anttax/anttaxlogo.001.jpeg' width="100%">
-    <img src='../../assets/anttax/anttaxlogo.002.jpeg' width="100%">
-    <img src='../../assets/anttax/anttaxlogo.003.jpeg' width="100%">
-    <img src='../../assets/anttax/anttaxlogo.004.jpeg' width="100%">
-    <img src='../../assets/anttax/anttaxlogo.005.jpeg' width="100%">
-    <img src='../../assets/anttax/anttaxlogo.006.jpeg' width="100%">
-    <img src='../../assets/anttax/anttaxlogo.007.jpeg' width="100%">
-    <img src='../../assets/anttax/anttaxlogo.008.jpeg' width="100%">
+  <div style="margin-bottom:33px;">
+    <h3>Logo提案</h3>
+    <img src='../../assets/anttax/anttaxlogo.001.jpeg' width="66%">
+    <img src='../../assets/anttax/anttaxlogo.002.jpeg' width="66%">
+    <img src='../../assets/anttax/anttaxlogo.003.jpeg' width="66%">
+    <img src='../../assets/anttax/anttaxlogo.004.jpeg' width="66%">
+    <img src='../../assets/anttax/anttaxlogo.005.jpeg' width="66%">
+    <img src='../../assets/anttax/anttaxlogo.006.jpeg' width="66%">
+    <img src='../../assets/anttax/anttaxlogo.007.jpeg' width="66%">
+    <img src='../../assets/anttax/anttaxlogo.008.jpeg' width="66%">
+    <h3>Logo定稿</h3>
+    <img src='../../assets/anttax/logofix1.png' width="33%">
+    <br>
+    <img src='../../assets/anttax/logofix2.png' width="33%">
+    <br>
+    <img src='../../assets/anttax/logofix3.png' width="33%">
   </div>
 </template>
 
@@ -28,5 +33,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  img{
+    border-bottom: 1px solid #bbb;
+    box-shadow: 0 0 7px #aaa;
+    padding:20px;
+  }
 </style>
