a3dbf756ce17dc194fd672399ef559c5a0932744
SimonSun <9262981@qq.com>
2018-12-25 20:13:31 +0800

    no message

--- src/components/apanel/newspublish.vue
+++ src/components/apanel/newspublish.vue
@@ -1,31 +1,34 @@
-<template>
-  <div class="routerview">
-      <h3>SEO官网文章发布方法 </h3>
+<style scoped>
 
-      <p><img src='../../assets/apanel/wenzhang1.png' width="95%"></p>
-      <p>1.后台依次点击左侧:资讯管理-右侧添加按钮打开文章发布页面;</p>
-      <p><img src='../../assets/apanel/wenzhang2.png' width="95%"></p>
-      <p>2.图二为文章发布页面</p>
-      <p>* 标题:文章标题文字个数最好在12-33个之间,尽量避免太短或者太长的标题个数;</p>
-      <p>* 关键词:关键词个数,最好在2-6个之间,避免太多或者太少;</p>
-      <p>* 描述文字:尽量在2-5句左右,尽量控制在120字以内;</p>
-      <h3>SEO官网文章发布注意事项 </h3>
-      <p>1.鼓励原创有质量的文章发布;</p>
-      <p>2.伪原创文章,一定记得在记事本中粘贴下文字,再去后台粘贴回来.</p>
-  </div>
+
+</style>
+
+<template>
+
+<div class="routerview">
+    <h3>SEO官网文章发布方法 </h3>
+
+    <p><img src='../../assets/apanel/wenzhang1.png' width="95%"></p>
+    <p>1.后台依次点击左侧:资讯管理-右侧添加按钮打开文章发布页面;</p>
+    <p><img src='../../assets/apanel/wenzhang2.png' width="95%"></p>
+    <p>2.图二为文章发布页面</p>
+    <p>* 标题:文章标题文字个数最好在12-33个之间,尽量避免太短或者太长的标题个数;</p>
+    <p>* 关键词:关键词个数,最好在2-6个之间,避免太多或者太少;</p>
+    <p>* 描述文字:尽量在2-5句左右,尽量控制在120字以内;</p>
+    <h3>SEO官网文章发布注意事项 </h3>
+    <p>1.鼓励原创有质量的文章发布;</p>
+    <p>2.伪原创文章,一定记得在记事本中粘贴下文字,再去后台粘贴回来.</p>
+</div>
+
 </template>
 
 <script>
+
 export default {
-  name: 'NewsPublish',
-  data() {
-    return {
+    name: 'NewsPublish',
+    data() {
+        return {}
     }
-  }
 }
+
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
-</style>
